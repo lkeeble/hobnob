289553ab4339b1be461f78ce8d659274ef906e62
Louis Keeble <lkeeble@yahoo.com>
2020-06-20 14:29:52 -0500

    blog entry

---
+++ blog/RoadToOblivion.php
@@ -0,0 +1,28 @@
+<?php
+$root = realpath($_SERVER["DOCUMENT_ROOT"]);
+include("$root/header.php");
+?>
+
+<body>
+
+<h1>
+06/20/2020 - Things that should be mentioned towards the beginning of a person's education
+</h1>
+
+<ul>
+  <li>Nobody understands why were are here and what the point of life is. "Life is a mystery, everyone must stand alone" (Madonna, in the song "Like a Prayer").</li>
+  <li>Confidence is the result of either trickery or delusion. Nobody has reason to be confident about anything (see above).</li>
+  <li>Much of life consists of figuring out a way to get things from other people.</li>
+  <li>Much of life consists of distractions that help people avoid thinking about death.</li>
+  <li>All religions are created and propagated by men.</li>
+  <li>We are all on the road to oblivion. The destination for all of us is the same whatever path we take.</li>
+  <li>Despite the above, it is possible to find happiness while we are alive.<li>
+  <li>To be continued... if you can think of other things send me an email :)</li>
+</ul>
+
+</body>
+
+
+<?php
+include("$root/footer.php");
+?>
