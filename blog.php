e6b6bb7fade0c1d8c849d7fc8266e9d0cab449a3
Louis Keeble <lkeeble@yahoo.com>
2020-06-20 14:36:02 -0500

    tweaks

--- blog.php
+++ blog.php
@@ -12,6 +12,10 @@ Blog
 <a href="blog/school.php">11/27/2019 - Some thoughts on the teaching of physics in school and university</p></a>
 </h2>
 
+<h2>
+<a href="blog/RoadToOblivion.php">06/20/2020 - Things that should be mentioned towards the beginning of a person's education</a>
+</h2>
+
 
 
 </body>
--- blog/RoadToOblivion.php
+++ blog/RoadToOblivion.php
@@ -13,11 +13,10 @@ include("$root/header.php");
   <li>Nobody understands why were are here and what the point of life is. "Life is a mystery, everyone must stand alone" (Madonna, in the song "Like a Prayer").</li>
   <li>Confidence is the result of either trickery or delusion. Nobody has reason to be confident about anything (see above).</li>
   <li>Much of life consists of figuring out a way to get things from other people.</li>
-  <li>Much of life consists of distractions that help people avoid thinking about death.</li>
-  <li>All religions are created and propagated by men.</li>
+  <li>Much of life consists of distractions that help people avoid thinking about death or convince them that death is OK.</li>
+  <li>All religions are created and propagated by men (see above).</li>
   <li>We are all on the road to oblivion. The destination for all of us is the same whatever path we take.</li>
   <li>Despite the above, it is possible to find happiness while we are alive.<li>
-  <li>To be continued... if you can think of other things send me an email :)</li>
 </ul>
 
 </body>
